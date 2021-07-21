a2edc73b7bc1ee5a0ca6a306d4a0fde05fbefcfa
Matt Hicks <matt@matthicks.com>
2021-07-21 10:33:04 -0700

    Better support for ValueTypes

--- core/shared/src/main/scala/fabric/ValueType.scala
+++ core/shared/src/main/scala/fabric/ValueType.scala
@@ -3,13 +3,13 @@ package fabric
 /**
  * ValueType represents the possible types of Value
  */
-sealed trait ValueType
+sealed trait ValueType[T]
 
 case object ValueType {
-  case object Obj extends ValueType
-  case object Arr extends ValueType
-  case object Str extends ValueType
-  case object Num extends ValueType
-  case object Bool extends ValueType
-  case object Null extends ValueType
+  case object Obj extends ValueType[fabric.Obj]
+  case object Arr extends ValueType[fabric.Arr]
+  case object Str extends ValueType[fabric.Str]
+  case object Num extends ValueType[fabric.Num]
+  case object Bool extends ValueType[fabric.Bool]
+  case object Null extends ValueType[fabric.Null]
 }
