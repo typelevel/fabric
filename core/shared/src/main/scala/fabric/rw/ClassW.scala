58cad7f6d83ba869f6ae94a7045f4098a4ae8199
Matt Hicks <matt@matthicks.com>
2022-10-01 07:41:01 -0500

    Added default Json Format for JsonParser

--- core/shared/src/main/scala/fabric/rw/ClassW.scala
+++ core/shared/src/main/scala/fabric/rw/ClassW.scala
@@ -7,5 +7,5 @@ import scala.collection.immutable.ListMap
 trait ClassW[T] extends Writer[T] {
   protected def map2T(map: ListMap[String, Json]): T
 
-  override def write(value: Json): T = map2T(value.asObj.value)
+  override def write(value: Json): T = map2T(value.asMap)
 }
