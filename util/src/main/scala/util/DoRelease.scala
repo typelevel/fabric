65934df4417be2939f40774947338011d5b453d0
Scala Steward <scala_steward@virtuslab.com>
2023-02-26 22:26:08 +0000

    Reformat with scalafmt 3.7.2
    Executed command: scalafmt --non-interactive

--- util/src/main/scala/util/DoRelease.scala
+++ util/src/main/scala/util/DoRelease.scala
@@ -45,8 +45,9 @@ object DoRelease {
       .readAllLines(file, Charset.forName("UTF-8"))
       .asScala
       .find(_.startsWith("ThisBuild / tlBaseVersion :="))
-      .map { s =>
-        s.substring(s.indexOf('"') + 1, s.lastIndexOf('"')).split('.')
+      .map {
+        s =>
+          s.substring(s.indexOf('"') + 1, s.lastIndexOf('"')).split('.')
       }
       .map(a => Version(a(0).toInt, a(1).toInt, 0))
       .getOrElse(
@@ -58,54 +59,37 @@ object DoRelease {
 
   private def nextVersion(): Version = {
     val result = "git tag".!!
-    result
-      .split('\n')
-      .toList
-      .map(_.trim)
-      .filter(_.nonEmpty)
-      .filter(_.startsWith("v"))
-      .map(Version.apply)
-      .max
-      .next
+    result.split('\n').toList.map(_.trim).filter(_.nonEmpty).filter(_.startsWith("v")).map(Version.apply).max.next
   }
 
-  private def gitCommit(version: Version): Unit = {
-    s"git commit -a -m \"Release $version\"".! match {
-      case 0 => // Success
-      case n => throw new RuntimeException(s"Git Commit Failure: $n")
-    }
+  private def gitCommit(version: Version): Unit = s"git commit -a -m \"Release $version\"".! match {
+    case 0 => // Success
+    case n => throw new RuntimeException(s"Git Commit Failure: $n")
   }
 
-  private def gitTag(version: Version): Unit = {
-    s"git tag $version".! match {
-      case 0 => // Success
-      case n => throw new RuntimeException(s"Git Tag Failure: $n")
-    }
+  private def gitTag(version: Version): Unit = s"git tag $version".! match {
+    case 0 => // Success
+    case n => throw new RuntimeException(s"Git Tag Failure: $n")
   }
 
-  private def gitPush(): Unit = {
-    "git push origin master --tags".! match {
-      case 0 => // Success
-      case n => throw new RuntimeException(s"Git Push Failure: $n")
-    }
+  private def gitPush(): Unit = "git push origin master --tags".! match {
+    case 0 => // Success
+    case n => throw new RuntimeException(s"Git Push Failure: $n")
   }
 
   private def updateReadme(version: Version): Unit = {
     val readme = Paths.get("README.md")
-    val text = new String(Files.readAllBytes(readme), "UTF-8")
-      .replace("$VERSION", version.number)
+    val text = new String(Files.readAllBytes(readme), "UTF-8").replace("$VERSION", version.number)
     Files.write(readme, text.getBytes("UTF-8"))
     ()
   }
 
-  private case class Version(major: Int, minor: Int, build: Int)
-      extends Ordered[Version] {
+  private case class Version(major: Int, minor: Int, build: Int) extends Ordered[Version] {
     def next: Version = copy(build = build + 1)
 
     override def compare(that: Version): Int = this.major - that.major match {
       case n if n != 0 => n
-      case _ =>
-        this.minor - that.minor match {
+      case _ => this.minor - that.minor match {
           case n if n != 0 => n
           case _ => this.build - that.build
         }
@@ -120,8 +104,7 @@ object DoRelease {
     private val Regex = """v?(\d+)[.](\d+)[.](\d+)""".r
 
     def apply(version: String): Version = version match {
-      case Regex(major, minor, build) =>
-        Version(major.toInt, minor.toInt, build.toInt)
+      case Regex(major, minor, build) => Version(major.toInt, minor.toInt, build.toInt)
       case _ => throw new RuntimeException(s"Invalid version: $version")
     }
   }
