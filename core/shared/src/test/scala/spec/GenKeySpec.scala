015bb9abbd07c87cf09ed41c8dabe44f32c9bdcf
Matt Hicks <matt@matthicks.com>
2023-04-11 07:58:56 -0500

    Release v1.10.6

---
+++ core/shared/src/test/scala/spec/GenKeySpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Typelevel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package spec
+
+import fabric._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class GenKeySpec extends AnyWordSpec with Matchers {
+  "JSON genKey" should {
+    "generate a simple key" in {
+      val j1 = obj(
+        "one" -> "Apple",
+        "two" -> arr(
+          "Banana",
+          "Cherry"
+        ),
+        "three" -> 3
+      )
+      val j2 = obj(
+        "two" -> arr(
+          "Banana",
+          "Cherry"
+        ),
+        "three" -> 3,
+        "one" -> "Apple"
+      )
+      println(j1.toKey)
+      j1 should be(j2)
+      j1.toString should not be j2.toString
+      j1.toKey should be(j2.toKey)
+    }
+  }
+}
