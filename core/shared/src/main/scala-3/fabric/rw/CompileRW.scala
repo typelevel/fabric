4a1732360cac08170f565034351628b53e6a0950
Matt Hicks <matt@outr.com>
2025-07-25 14:14:54 -0500

    Added proper abstract derivation support for Scala 3 via RW.gen

--- core/shared/src/main/scala-3/fabric/rw/CompileRW.scala
+++ core/shared/src/main/scala-3/fabric/rw/CompileRW.scala
@@ -41,19 +41,15 @@ trait CompileRW {
 
   inline final def derived[T <: Product](using inline T: Mirror.ProductOf[T], ct: ClassTag[T]): RW[T] = gen[T]
 
-  inline def gen[T <: Product](using Mirror.ProductOf[T], ClassTag[T]): RW[T] = new ClassRW[T] {
-    override protected def t2Map(t: T): Map[String, Json] = toMap(t)
-    override protected def map2T(map: Map[String, Json]): T = fromMap[T](map)
-    override def definition: DefType = toDefinition[T]
-  }
+  inline def gen[T <: Product](using Mirror.ProductOf[T], ClassTag[T]): RW[T] =
+    ${ CompileRW.genMacro[T] }
 
   inline def genR[T <: Product](using Mirror.ProductOf[T]): Reader[T] = new ClassR[T] {
     override protected def t2Map(t: T): Map[String, Json] = toMap(t)
   }
 
-  inline def genW[T <: Product](using Mirror.ProductOf[T]): Writer[T] = new ClassW[T] {
-    override protected def map2T(map: Map[String, Json]): T = fromMap[T](map)
-  }
+  inline def genW[T <: Product](using Mirror.ProductOf[T]): Writer[T] =
+    ${ CompileRW.genWMacro[T] }
 
   inline def toClassName[T](using ct: ClassTag[T]): Option[String] =
     Some(ct.runtimeClass.getName.replace("$", "."))
@@ -138,12 +134,10 @@ trait CompileRW {
               case Null if default.nonEmpty => default.get
               case json => hdWritable.write(json)
             }.getOrElse(default.get)
-//            val valueOption: Option[hd] = hdValueOption.map(hdWritable.write)
-//            val value = valueOption.getOrElse(default)
             arr(index) = value
             fromMapElems[A, tl, tlLabels](map, index + 1, arr, defaults)
           case EmptyTuple => sys.error("Not possible")
-      }
+        }
       case EmptyTuple => // Finished
     }
   }
@@ -153,7 +147,7 @@ trait CompileRW {
   inline def getClassName[T]: String = ${ CompileRW.getClassNameImpl[T] }
 }
 
-object CompileRW {
+object CompileRW extends CompileRW {
   def getDefaultParmasImpl[T](using Quotes, Type[T]): Expr[Map[String, AnyRef]] = {
     import quotes.reflect._
     val sym = TypeTree.of[T].symbol
@@ -162,13 +156,13 @@ object CompileRW {
       val comp = if (sym.isClassDef) sym.companionClass else sym
       val names =
         for p <- sym.caseFields if p.flags.is(Flags.HasDefault)
-        yield p.name
+          yield p.name
       val namesExpr: Expr[List[String]] = Expr.ofList(names.map(Expr(_)))
 
       val body = comp.tree.asInstanceOf[ClassDef].body
       val idents: List[Ref] =
         for case deff @ DefDef(name, _, _, _) <- body
-        if name.startsWith("$lessinit$greater$default")
+            if name.startsWith("$lessinit$greater$default")
         yield Ref(deff.symbol)
       val identsExpr: Expr[List[Any]] =
         Expr.ofList(idents.map(_.asExpr))
@@ -183,5 +177,166 @@ object CompileRW {
     import quotes.reflect._
 
     Expr(TypeTree.of[T].symbol.companionClass.fullName)
+  }
+
+  def genMacro[T <: Product: Type](using Quotes): Expr[RW[T]] = {
+    import quotes.reflect._
+
+    val tpe = TypeRepr.of[T]
+    val typeSymbol = tpe.typeSymbol
+    val fields = typeSymbol.caseFields
+    val companion = typeSymbol.companionModule
+
+    val mirror = Expr.summon[Mirror.ProductOf[T]].getOrElse {
+      report.errorAndAbort(s"No Mirror.ProductOf found for ${Type.show[T]}")
+    }
+    val ct = Expr.summon[ClassTag[T]].getOrElse {
+      report.errorAndAbort(s"No ClassTag found for ${Type.show[T]}")
+    }
+
+    '{
+      new ClassRW[T] {
+        override protected def t2Map(t: T): Map[String, Json] = CompileRW.toMap(t)(using $mirror)
+
+        override protected def map2T(map: Map[String, Json]): T = {
+          ${ generateDirectConstructor[T]('{map}) }
+        }
+
+        override def definition: DefType = CompileRW.toDefinition[T](using $mirror, $ct)
+      }
+    }
+  }
+
+  def genWMacro[T <: Product: Type](using Quotes): Expr[Writer[T]] = {
+    import quotes.reflect._
+
+    '{
+      new ClassW[T] {
+        override protected def map2T(map: Map[String, Json]): T = {
+          ${ generateDirectConstructor[T]('{map}) }
+        }
+      }
+    }
+  }
+
+  private def generateDirectConstructor[T <: Product: Type](map: Expr[Map[String, Json]])(using Quotes): Expr[T] = {
+    import quotes.reflect._
+
+    val tpe = TypeRepr.of[T]
+    val typeSymbol = tpe.typeSymbol
+
+    if (!typeSymbol.isClassDef || !typeSymbol.flags.is(Flags.Case)) {
+      report.errorAndAbort(s"${typeSymbol.name} is not a case class")
+    }
+
+    val fields = typeSymbol.caseFields
+    val companionModule = typeSymbol.companionModule
+
+    // Check if T is a JsonWrapper
+    val isJsonWrapperType = tpe <:< TypeRepr.of[JsonWrapper]
+
+    // Get default values
+    val defaultsExpr = getDefaultParmasImpl[T]
+
+    // Generate field extraction expressions
+    val fieldExprs = fields.map { field =>
+      val fieldName = field.name
+      val fieldTypeRepr = tpe.memberType(field)
+
+      fieldTypeRepr.asType match {
+        case '[ft] =>
+          val writer = Expr.summon[Writer[ft]].getOrElse {
+            report.errorAndAbort(s"No Writer found for field $fieldName of type ${Type.show[ft]}")
+          }
+
+          val isOptional = fieldTypeRepr <:< TypeRepr.of[Option[_]]
+          val fieldNameStr = Expr(fieldName)
+          val isJsonWrapperExpr = Expr(isJsonWrapperType)
+          val classNameExpr = Expr(typeSymbol.fullName)
+
+          '{
+            val defaults = $defaultsExpr
+            val jsonOpt = $map.get($fieldNameStr)
+            val defaultOpt = defaults.get($fieldNameStr)
+
+            jsonOpt match {
+              case Some(json) =>
+                json match {
+                  case fabric.Null if defaultOpt.isDefined => defaultOpt.get.asInstanceOf[ft]
+                  case _ => $writer.write(json)
+                }
+              case None =>
+                if ($fieldNameStr == "json" && $isJsonWrapperExpr) {
+                  $writer.write(fabric.Obj($map))
+                } else {
+                  defaultOpt match {
+                    case Some(defaultValue) => defaultValue.asInstanceOf[ft]
+                    case None =>
+                      ${
+                        if (isOptional) {
+                          '{ None.asInstanceOf[ft] }
+                        } else {
+                          '{ throw fabric.rw.RWException(s"Unable to find field ${$classNameExpr}.${$fieldNameStr} (and no defaults set) in ${fabric.Obj($map)}") }
+                        }
+                      }
+                  }
+                }
+            }
+          }
+      }
+    }
+
+    // For case classes, we need to use the companion object's apply method
+    // Get the companion object reference based on the type
+    val companionRef = tpe match {
+      case AppliedType(tycon, typeArgs) =>
+        // For generic types like OrganizationDetail[Org], we need to get the companion
+        // and potentially apply type arguments
+        Ref(companionModule)
+      case _ =>
+        // For non-generic types
+        Ref(companionModule)
+    }
+
+    // Find the apply method in the companion
+    val applyMethods = companionModule.methodMember("apply")
+
+    // Find the correct apply method by checking parameter count
+    val applyMethod = applyMethods.find { method =>
+      method.paramSymss match {
+        case typeParams :: valueParams :: Nil if typeParams.forall(_.isType) =>
+          // Generic apply method with type parameters
+          valueParams.length == fields.length
+        case valueParams :: Nil if !valueParams.exists(_.isType) =>
+          // Non-generic apply method
+          valueParams.length == fields.length
+        case _ => false
+      }
+    }.getOrElse {
+      report.errorAndAbort(s"No suitable apply method found in companion object of ${typeSymbol.name}")
+    }
+
+    // Create the method call
+    val methodCall = Select(companionRef, applyMethod)
+
+    // Apply type arguments if needed
+    val appliedMethod = tpe match {
+      case AppliedType(_, typeArgs) if applyMethod.paramSymss.headOption.exists(_.forall(_.isType)) =>
+        // The apply method has type parameters, apply them
+        val typeTreeArgs = typeArgs.map { arg =>
+          arg.asType match {
+            case '[t] => TypeTree.of[t]
+          }
+        }
+        TypeApply(methodCall, typeTreeArgs)
+      case _ =>
+        // No type parameters needed
+        methodCall
+    }
+
+    // Apply the value arguments
+    val constructorCall = Apply(appliedMethod, fieldExprs.map(_.asTerm))
+
+    constructorCall.asExprOf[T]
   }
 }
