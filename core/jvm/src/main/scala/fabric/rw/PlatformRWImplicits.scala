74de53af19c51755220500e9da154ef12b888e00
Matt Hicks <matt@outr.com>
2026-03-31 13:15:28 -0500

    Added `PlatformRWImplicits` for platform-specific read-write implicits and integrated it into `RWImplicits`

---
+++ core/jvm/src/main/scala/fabric/rw/PlatformRWImplicits.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Typelevel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package fabric.rw
+
+import fabric._
+import fabric.define.DefType
+
+import java.io.File
+import java.nio.file.{Path, Paths}
+
+trait PlatformRWImplicits {
+  implicit lazy val pathRW: RW[Path] = RW.from[Path](
+    r = p => str(p.toString),
+    w = j => Paths.get(j.asString),
+    d = DefType.Str
+  )
+
+  implicit lazy val fileRW: RW[File] = RW.from[File](
+    r = f => str(f.getPath),
+    w = j => new File(j.asString),
+    d = DefType.Str
+  )
+}
